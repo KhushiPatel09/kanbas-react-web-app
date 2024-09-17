9c2eaedb7aa459259e61a2f53ea75f7f90f621b2
Khushi Patel <khushipatel@Khushis-MBP.ht.home>
2024-09-17 14:45:57 -0400

    Made kanbas as default page

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -7,10 +7,13 @@ import Lab3 from "./Lab3";
 export default function Labs() {
   return (
     <div>
+      <h1>Khushi Patel</h1>
+      <h2>CS5610 Section-1</h2>
+      <h3></h3>
       <h1>Labs</h1>
       <TOC />
       <Routes>
-        <Route path="/" element={<Navigate to="Lab1" />} />
+        {/* <Route path="/" element={<Navigate to="Lab1" />} /> */}
         <Route path="Lab1" element={<Lab1 />} />
         <Route path="Lab2" element={<Lab2 />} />
         <Route path="Lab3" element={<Lab3 />} />
