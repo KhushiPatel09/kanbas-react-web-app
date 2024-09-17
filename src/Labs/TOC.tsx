3f12fcac29b9e48c1cdcf478efb9d4e87b427f1a
Khushi Patel <khushipatel@Khushis-MBP.ht.home>
2024-09-17 15:18:39 -0400

    final changes in assignment-1

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 export default function TOC() {
     return (
       <ul>
-        <li id="wd-github"><Link to="https://github.com/KhushiPatel09/kanbas-react-web-app.git">Github</Link></li>
+        <li id="wd-github"><Link to="https://github.com/KhushiPatel09/kanbas-react-web-app.git" target="_blank">Github</Link></li>
         <li><Link to="/Labs">Labs</Link></li>
         <li><Link to="/Labs/Lab1">Lab 1</Link></li>
         <li><Link to="/Labs/Lab2">Lab 2</Link></li>
